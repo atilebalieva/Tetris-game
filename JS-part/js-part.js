bada26c5c41719acdb724f176007ce8a99853add
aitilebalieva <aitilebalieva@gmail.com>
2022-11-16 15:49:17 -0600

    Updated [Tetris] repo v0.1

---
+++ JS-part/js-part.js
@@ -0,0 +1,236 @@
+function log(func) {
+  console.log(func);
+}
+
+const playArea = document.getElementById("play-area");
+const defaultColor = "rgb(25, 25, 65)";
+const playButton = document.getElementById("play-button");
+
+let currentShape = null;
+let currentPosition = 1;
+
+let changeRowPosition = 0;
+let changeColumnPosition = 4;
+
+let randomRow = Math.floor(Math.random() * 17);
+let randomColumn = Math.floor(Math.random() * 7);
+log(randomColumn);
+const divArray = new Array(20);
+
+const modelArray = [
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+];
+
+const allShapes = {
+  L: {
+    1: [
+      [1, 0],
+      [1, 0],
+      [1, 1],
+    ],
+    2: [
+      [1, 1, 1],
+      [1, 0, 0],
+    ],
+    3: [
+      [1, 1],
+      [0, 1],
+      [0, 1],
+    ],
+    4: [
+      [0, 0, 1],
+      [1, 1, 1],
+    ],
+  },
+  Z: {
+    1: [
+      [1, 1, 0],
+      [0, 1, 1],
+    ],
+    2: [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ],
+  },
+  I: {
+    1: [[1, 1, 1, 1]],
+    2: [[1], [1], [1], [1]],
+  },
+  O: {
+    1: [
+      [1, 1],
+      [1, 1],
+    ],
+  },
+  T: {
+    1: [
+      [0, 1, 0],
+      [1, 1, 1],
+    ],
+    2: [
+      [1, 0],
+      [1, 1],
+      [1, 0],
+    ],
+    3: [
+      [1, 1, 1],
+      [0, 1, 0],
+    ],
+    4: [
+      [0, 1],
+      [1, 1],
+      [0, 1],
+    ],
+  },
+  S: {
+    1: [
+      [0, 1, 1],
+      [1, 1, 0],
+    ],
+    2: [
+      [1, 0],
+      [1, 1],
+      [0, 1],
+    ],
+  },
+};
+
+function createNewDivCell() {
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("cellStyle");
+  return playArea.appendChild(newDiv);
+}
+
+for (let i = 0; i < divArray.length; i++) {
+  divArray[i] = Array.from({ length: 10 }).map(function (el) {
+    el = createNewDivCell();
+    return el;
+  });
+}
+
+function refreshDivArray() {
+  let color = random_bg_color();
+
+  for (let i = 0; i < modelArray.length; i++) {
+    for (let j = 0; j < modelArray[i].length; j++) {
+      if (modelArray[i][j] === "" || modelArray[i][j] === 0) {
+        divArray[i][j].style.background = defaultColor;
+      } else if (color === defaultColor) {
+        color = "green";
+        divArray[i][j].style.background = color;
+      } else {
+        divArray[i][j].style.background = color;
+      }
+    }
+  }
+}
+
+function cleanModelArray() {
+  for (let i = 0; i < modelArray.length; i++) {
+    for (let j = 0; j < modelArray[i].length; j++) {
+      if (modelArray[i][j] === 1) {
+        modelArray[i][j] = "";
+      }
+    }
+  }
+}
+
+function random_bg_color() {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  return "rgb(" + x + "," + y + "," + z + ")";
+}
+
+function setCurrentShape() {
+  let getKeysInAllShapes = Object.keys(allShapes);
+  let randomKeys =
+    getKeysInAllShapes[Math.floor(Math.random() * getKeysInAllShapes.length)];
+  currentShape = allShapes[randomKeys];
+}
+
+window.addEventListener("keydown", (e) => {
+  handleKeyDown(e);
+});
+
+function handleKeyDown(e) {
+  if (e.code === "ArrowUp") {
+    handleArrowUp();
+    copyCurrentShapeToModelArray();
+  }
+  if (e.code === "ArrowDown") {
+    handleArrowDown();
+    copyCurrentShapeToModelArray();
+  }
+  if (e.code === "ArrowLeft") {
+    handleArrowLeft();
+    copyCurrentShapeToModelArray();
+  }
+  if (e.code === "ArrowRight") {
+    handleArrowRight();
+    copyCurrentShapeToModelArray();
+  }
+}
+
+function handleArrowUp() {
+  const positionCount = Object.keys(currentShape).length;
+
+  currentPosition++;
+
+  if (currentPosition > positionCount) {
+    currentPosition = 1;
+  }
+}
+// let changeRowPosition = 0;
+// let changeColumnPosition = 4;
+
+function handleArrowDown() {
+  changeRowPosition++;
+}
+
+function handleArrowLeft() {
+  // if()
+  changeColumnPosition--;
+}
+
+function handleArrowRight() {
+  changeColumnPosition++;
+}
+
+function copyCurrentShapeToModelArray() {
+  cleanModelArray();
+  const shape = currentShape[currentPosition];
+
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[i].length; j++) {
+      modelArray[16 + i][8 + j] = shape[i][j];
+    }
+  }
+
+  refreshDivArray();
+}
+
+playButton.addEventListener("click", () => {
+  setCurrentShape();
+  copyCurrentShapeToModelArray();
+});
